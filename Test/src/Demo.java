7f70387a8c5de5a903b3d9d0f810b4271eabdd4a
204419 <204419@MC02LAP0014.Mercator.com>
2018-12-06 17:00:53 +0530

    Updated

---
+++ Test/src/Demo.java
@@ -0,0 +1,14 @@
+import com.service.Sonawane;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+
+	System.out.println(new Sonawane().helloAnkita());
+	System.out.println("Hello !");
+	
+	System.out.println("Hi !!!");
+	
+	}
+
+}
